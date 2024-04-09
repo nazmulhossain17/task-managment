5e5fa88da9392ea0294646c45b3069472d38ff38
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-04-09 23:25:45 +0600

    dashboard-design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Loader from "./common/Loader";
 import Login from "./components/Auth/Login/Login";
 import Register from "./components/Auth/Register/Register";
+import DefaultLayout from "./Pages/DefaultLayout/DefaultLayout";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -17,6 +18,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/sign-up" element={<Register />} />
+      <Route path="/dashboard" element={<DefaultLayout />} />
     </Routes>
   );
 }
